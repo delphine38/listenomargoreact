4920681b4887f63520874d70ea82768f62d1a60c
delphine <delphin38@live.fr>
2022-05-16 16:44:20 +0200

    read and delete ok

--- server/index.js
+++ server/index.js
@@ -1,6 +1,55 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'listenomargoreact',
+    password: 'listenomargoreact',
+    database: 'listenomargoreact',
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/get', (req, res)=>{
+    const sqlSelect = "SELECT * FROM name";
+    db.query(sqlSelect, (err, result)=>{
+        // console.log(result);
+        res.send(result);
+    });
+});
+
+app.delete('/api/delete/:name', (req, res)=>{
+    const name = req.params.name
+    const sqlDelete = "DELETE FROM name WHERE name = ?";
+
+    db.query(sqlDelete, name, (err, result)=>{
+     if(err) console.log(err)
+    });
+});
+
+app.post("/api/insert", (req, res)=>{
+    const name = req.body.name;
+
+    const sqlInsert = "INSERT INTO name (name) VALUES (?);"
+    db.query(sqlInsert, [name], (err, result)=>{
+        console.log(err);
+    });
+});
+// app.get('/', (req, res)=>{
+    
+//     // ceci est une ligne de test
+//     // const sqlInsert = "INSERT INTO name (name) VALUES ('Delphine');"
+//     // db.query(sqlInsert, (err, result)=>{
+//     //     res.send("hello soleil");
+//     // });
+    
+// });
 
 app.listen(3001, () => {
     console.log("cela fonctionne sur le port 30001");
-})
+});
--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-// import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+// import { useEffect } from 'react';
 import './App.css';
+import Axios from "axios";
 
 function App() {
+
+const [name, setName] = useState('');
+const [liste, setList] = useState([]);
+
+useEffect(()=>{
+  Axios.get("http://localhost:3001/api/get")
+  .then((Response)=>{
+    console.log(Response.data);
+    setList(Response.data)
+  })
+},[]);
+
+const submitName = () => {
+  Axios.post("http://localhost:3001/api/insert", {
+    name: name
+  }).then(()=>{
+    alert("Bravo le nom est insérer");
+  });
+  setList([
+    ...liste,{name: name},
+  ]);
+
+};
+
+const deleteName = (nam) => {
+  Axios.delete(`http://localhost:3001/api/delete/${nam}`);
+};
+
   return (
     <div className="App">
       <h1>Liste de noms des argonautes React, node, mysql</h1>
 
-      <input type="text" name="name" placeholder='Insérer un nom' />
-      <button>Envoyer</button>
+      <input 
+        type="text" 
+        name="name" 
+        placeholder='Insérer un nom' 
+        onChange={(e)=> {
+        setName(e.target.value);
+      }} />
+      <button className="green" onClick={submitName}>Envoyer</button>
+
+
+      {/* affichage de la donnée */}
+      {liste.map((value)=>{
+        return(
+          <div>
+            <h3>{value.name}</h3>
+
+            <h4 className="colorgreen"><u>Rafraichi</u> la page pour <u>voir</u> les résultats de la <u>modification</u> ou de la <u>suppression</u></h4>
+
+
+            <button onClick={()=>{deleteName(value.name)}} id="deleteName" className="delete">Supprimer</button>
+
+            <input placeholder="updateInput"></input>
+            {/* <input type="text" id="updateInput" noChange={(e)=>{
+              setName(e.target.value);
+            }} /> */}
+
+            <button className="update">Modifier</button>
+
+          </div>
+        )
+      })}
     </div>
   );
 }
