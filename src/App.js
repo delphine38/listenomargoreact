91636e7b28407465a558cacc3ab59beae213cb0c
delphine <delphin38@live.fr>
2022-05-16 10:02:07 +0200

    server

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Liste de noms des argonautes React, node, mysql</h1>
+
+      <input type="text" name="name" placeholder='Insérer un nom' />
+      <button>Envoyer</button>
     </div>
   );
 }
